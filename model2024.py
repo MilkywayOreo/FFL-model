e543c452dfd1f83f32bce8aba42f8b40c2e29f53
MilkywayOreo <max97hirsch@gmail.com>
2024-06-11 23:48:31 +0200

    neuer

--- model2024.py
+++ model2024.py
@@ -14,7 +14,6 @@ Kxy = 1  # equilibrium constants
 Kxz = 1
 Kyz = 1
 
-# simple Activator
 def f_activator(u, K, H):
     return (u/K)**H / (1 + (u/K)**H)
 
@@ -25,7 +24,6 @@ def Sx(t):
     else:
         return 0
 
-# dy / dt = f(t, y), y(t0) = y0, beware of argument order
 def ODE_Y(t, initial_values, By, betay, Kxy, H, alphay):
     x, y, z = initial_values  # entpacken von x,y,z
     x = Sx(t)
@@ -47,7 +45,6 @@ def simp_reg(t, initial_values, Bz, betaz, Kxz, Kyz, H, alphaz):
     return [dzdt]
 
 
-# Anfangswerte
 initial_values = [1, By, Bz]
 
 # Zeitbereich für die Lösung
@@ -56,28 +53,23 @@ t_eval = np.linspace(0, 15, 100)
 
 # ODEs lösen
 solution_y = solve_ivp(ODE_Y, t_span, initial_values, t_eval=t_eval, args=(By, betay, Kxy, H, alphay))
-print(solution_y)
 solution_z = solve_ivp(ODE_Z, t_span, initial_values, t_eval=t_eval, args=(Bz, betaz, Kxz, Kyz, H, alphaz))
-
-
 solution_simp_reg = solve_ivp(simp_reg, t_span, initial_values, t_eval=t_eval, args=(Bz, betaz, Kxz, Kyz, H, alphaz))
 
 
 # Plotten der Ergebnisse
-fig, axs = plt.subplots(2, 1, sharex=True, figsize=(8, 6))
+fig, axs = plt.subplots(2, 1, figsize=(8, 8))
 
-# Plotten von Sx
-sx_values = [Sx(t) for t in t_eval]
-axs[0].plot(t_eval, sx_values, label='Sx', color='red')
+Sx = [Sx(t) for t in t_eval]
+axs[0].plot(t_eval, Sx, label='Sx', color='red')
 axs[0].set_ylabel('Sx')
 axs[0].legend()
 
-# Plotten von Y und Z
 axs[1].plot(solution_y.t, solution_y.y[0], label='Y')
 axs[1].plot(solution_z.t, solution_z.y[0], label='Z')
 axs[1].plot(solution_simp_reg.t, solution_simp_reg.y[0], label='simp_reg')
-axs[1].set_xlabel('Time')
-axs[1].set_ylabel('Concentrations')
+axs[1].set_xlabel('Zeit [t]')
+axs[1].set_ylabel('Konzentrationen')
 axs[1].legend()
 
 plt.tight_layout()
